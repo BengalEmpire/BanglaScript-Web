01c615655ec8b7033510beefbb8c528109d23c19
Mahmudur Rahman <114731414+mahmud-r-farhan@users.noreply.github.com>
2025-10-08 13:09:45 +0600

    remove trash

--- frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-export const useStore = create((set) => ({
-  user: null,
-  group: null,
-  setUser: (user) => set({ user }),
-  setGroup: (group) => set({ group }),
-}));
